994091024c478d4a349a7a862915dcef3404db0e
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-05-30 15:31:26 -0500

    chore(nix/devShell): add RUST_SRC variable

--- flake-parts/shells.nix
+++ flake-parts/shells.nix
@@ -7,9 +7,12 @@
     self',
     ...
   }: let
+    inherit (self'.packages) rust-toolchain;
+    inherit (self'.legacyPackages) cargoExtraPackages ciPackages;
+
     devTools = [
       # rust tooling
-      self'.packages.rust-toolchain
+      rust-toolchain
       pkgs.cargo-audit
       pkgs.cargo-udeps
       pkgs.bacon
@@ -18,13 +21,13 @@
       self'.packages.treefmt
       # misc
     ];
-
-    inherit (self'.legacyPackages) cargoExtraPackages ciPackages;
-  in rec {
+  in {
     devShells = {
       default = pkgs.mkShell rec {
         packages = devTools ++ cargoExtraPackages ++ ciPackages;
+
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
+        RUST_SRC_PATH = "${self'.packages.rust-toolchain}/lib/rustlib/src/rust/src";
 
         shellHook = ''
           ${config.pre-commit.installationScript}
