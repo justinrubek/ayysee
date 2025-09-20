b7d31a93089f9dcfcb151eabebbdb3ab4ca96190
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-09-20 11:17:57 -0500

    build(nix): nix flake update

--- flake-parts/cargo.nix
+++ flake-parts/cargo.nix
@@ -13,7 +13,7 @@
     ];
     withExtraPackages = base: base ++ extraPackages;
 
-    craneLib = inputs.crane.lib.${system}.overrideToolchain self'.packages.rust-toolchain;
+    craneLib = (inputs.crane.mkLib pkgs).overrideToolchain self'.packages.rust-toolchain;
 
     common-build-args = rec {
       src = inputs.nix-filter.lib {
